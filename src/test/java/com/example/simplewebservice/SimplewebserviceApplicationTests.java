1a8e572e96c45e314ffa0394773c1a7a1278191a
Harsh Vardhan <harsh.vardhan02@nagarro.com>
2024-09-13 15:56:28 +0530

    Added simple webservices

---
+++ src/test/java/com/example/simplewebservice/SimplewebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
